345276e7733baa5a2d857d18289d16ddab1af41b
gayatri-innotech <gayatri@innotechteam.in>
2022-09-22 23:49:54 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-
+import { useState } from 'react';
 import './App.css';
 import TodoContainer from './containers/todoContainer';
+import {FaPlusCircle} from 'react-icons/fa'
+import Modal from './components/Modal';
 
-function App() {
+const App = () => {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <div className="App">
+      <div className="icons">
+      <FaPlusCircle className="openModalBtn" onClick={()=>{
+      setOpenModal(true);
+      }} />
       <h1 className="heading">Today</h1>
-      <i className="fa-regular fa-circle-plus" />
       <TodoContainer/>
+      {openModal && <Modal closeModal={setOpenModal}/>}
+      </div>
     </div>
   );
 }
--- src/components/todoList/index.js
+++ src/components/todoList/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './style.css'
+
 
 const Todo=({todo})=>{
     console.log(todo)
     return (
-        <div>
-            <input className="ckeck" type="checkbox"/>
+        <div className="box">
+            <input className="check" type="checkbox"/>
             <span>{todo.title}</span>
             <hr/>
         </div>
